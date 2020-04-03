15ac0b650be7d62e40b202b3f15c97849f1c5b5b
annaokragly <anna.okragly@gmail.com>
2020-04-03 15:23:05 -0400

    updating redux and firebase, adding hoc pattern and asynchronous redux and creating user and cart sagas for sign in and sign up

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -10,41 +10,79 @@ const config = {
     storageBucket: "ecommerce-app-dbb.appspot.com",
     messagingSenderId: "138886627858",
     appId: "1:138886627858:web:b6166ddc122c2db77c8ecb"
-  };
+};
 
-  export const createUserProfileDocument = async (userAuth, additionalData) => {
-      if (!userAuth) return;
+firebase.initializeApp(config);
 
-      const userRef = firestore.doc(`users/${userAuth.uid}`);
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+    if (!userAuth) return;
+    const userRef = firestore.doc(`users/${userAuth.uid}`);
 
-      const snapshot = await userRef.get();
+    const snapshot = await userRef.get();
 
-      if (!snapshot.exists) {
-          const { displayName, email } = userAuth;
-          const createdAt = new Date();
+    if (!snapshot.exists) {
+        const { displayName, email } = userAuth;
+        const createdAt = new Date();
 
-          try {
-              await userRef.set({
-                  displayName,
-                  email,
-                  createdAt,
-                  ...additionalData
-              })
-          } catch (error) {
-              console.log('error creating user', error.message);
-          }
-      }
+        try {
+            await userRef.set({
+                displayName,
+                email,
+                createdAt,
+                ...additionalData
+            })
+        } catch (error) {
+            console.log('error creating user', error.message);
+        }
+    }
 
-      return userRef;
-  }
+    return userRef;
+};
 
-  firebase.initializeApp(config);
+export const addCollectionAndDocuments = async (collectionKey, objectsToAdd) => {
+    const collectionRef = firestore.collection(collectionKey);
 
-  export const auth = firebase.auth();
-  export const firestore = firebase.firestore();
+    const batch = firestore.batch();
+    objectsToAdd.forEach(obj => {
+        const newDocRef = collectionRef.doc();
+        batch.set(newDocRef, obj);
+    });
 
-  const provider = new firebase.auth.GoogleAuthProvider();
-  provider.setCustomParameters({ prompt: 'select_account' });
-  export const signInWithGoogle = () => auth.signInWithPopup(provider);
+    return await batch.commit();
+};
 
-  export default firebase;
+export const convertCollectionsSnapshotToMap = (collections) => {
+    const transformedCollection = collections.docs.map(doc => {
+        const { title, items } = doc.data();
+
+        return {
+            routeName: encodeURI(title.toLowerCase()),
+            id: doc.id,
+            title,
+            items
+        };
+    });
+
+    return transformedCollection.reduce((accumulator, collection) => {
+        accumulator[collection.title.toLowerCase()] = collection;
+        return accumulator;
+    } , {});
+};
+
+export const getCurrentUser = () => {
+    return new Promise((resolve, reject) => {
+        const unsubscribe = auth.onAuthStateChanged(userAuth => {
+            unsubscribe();
+            resolve(userAuth);
+        }, reject)
+    })
+}
+
+export const auth = firebase.auth();
+export const firestore = firebase.firestore();
+
+export const googleProvider = new firebase.auth.GoogleAuthProvider();
+googleProvider.setCustomParameters({ prompt: 'select_account' });
+export const signInWithGoogle = () => auth.signInWithPopup(googleProvider);
+
+export default firebase;
